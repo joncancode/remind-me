e6abf2fa8512a306ecde26e10634f20dd3b85769
joncancode <jc.haines19@gmail.com>
2017-10-18 20:46:13 -0400

    delete function working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,19 @@
-export const ADD_REMINDER = 'ADD_REMINDER'
+export const ADD_REMINDER = 'ADD_REMINDER';
 export const addReminder = text => {
+  const action = {
+    type: ADD_REMINDER,
+    text
+  };
+  console.log('action in addreminder fun', action);
+  return action;
+};
 
-    const action = {
-        type: ADD_REMINDER,
-        text
-    }
-    console.log('action in addreminder fun', action)
-    return action;
-}
+export const DELETE_REMINDER = 'DELETE_REMINDER';
+export const deleteReminder = id => {
+  const action = {
+    type: DELETE_REMINDER,
+    id
+  };
+  console.log('delete in actions', action)
+  return action;
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_REMINDER } from '../actions/index';
+import { ADD_REMINDER, DELETE_REMINDER } from '../actions/index';
 
 const reminder = action => {
   return {
@@ -7,12 +7,21 @@ const reminder = action => {
   };
 };
 
+const removeById = (state = [], id) => {
+  const reminders = state.filter(reminder => reminder.id !== id);
+  console.log('new reduc remind', reminders);
+  return reminders;
+};
+
 const reminders = (state = [], action) => {
   let reminders = null;
   switch (action.type) {
     case ADD_REMINDER:
       reminders = [...state, reminder(action)];
       console.log('reminders as state', reminders);
+      return reminders;
+    case DELETE_REMINDER:
+      reminders = removeById(state, action.id);
       return reminders;
     default:
       return state;
